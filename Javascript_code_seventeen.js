6fe6290aaa360d977ba4d81e5faac2e6ab0015e6
Pranav Ruparelia <pranavruparelia9@gmail.com>
2019-11-11 17:50:45 -0500

    Javascript Date Function
    To get the day from the given dateString by converting the string first to date and then extracting the day from that value

---
+++ Javascript_code_seventeen.js
@@ -0,0 +1,35 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // Write your code here
+    var date = new Date(dateString);
+    dayName = days[date.getDay(date)];
+    
+    return dayName;
+}
+
